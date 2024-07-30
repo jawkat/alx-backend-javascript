a37b5ad4808d0f707a9278b348dd18ae3e20ae8a
jawkat <kad.joliano@gmail.com>
2024-07-31 00:32:16 +0100

    task 0 and 1 and 2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,3 @@
-import getListStudents from './0-get_list_students';
-
 export default function getStudentsByLocation(students, city) {
-  const locationStudents = getListStudents();
-
-  return locationStudents.filter((std) => std.location === city);
+  return students.filter((std) => std.location === city);
 }
